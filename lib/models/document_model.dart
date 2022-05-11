e6ad0db9f229e72de3f5d86ce9c600402d1be758
Mwita04 <emcee779@gmal.com>
2022-05-11 14:24:37 +0300

    commit on master

--- lib/models/document_model.dart
+++ lib/models/document_model.dart
@@ -9,8 +9,20 @@ class Document {
   Document(this.doc_title, this.doc_url, this.doc_date, this.page_num);
 
   static List<Document> doc_list = [
-    Document("doc_title", "doc_url", "doc_date", " page_num"),
-    Document("doc_title", "doc_url", "doc_date", "page_num"),
-    Document("doc_title", "doc_url", "doc_date", "page_num"),
+    Document(
+        "The 48 Laws Of Power",
+        "https://drive.google.com/file/d/1DuYMP2tMy7ncV3UiUIzrIBSzSxt6yvYl/view",
+        "2019-12-23",
+        "476"),
+    Document(
+        " The Way of the Superior Man",
+        "https://docs.google.com/file/d/0B8kQliclrbXrYnFkLTVTTFh6aGs/edit?pli=1&resourcekey=0-U7SZgmiRvm1H5XYf5L193Q",
+        "2021-05-23",
+        "146"),
+    Document(
+        "Think Like a Monk",
+        "https://icrrd.com/media/16-05-2021-072848Think-Like-a-Monk.pdf",
+        "2017-10-23",
+        "246"),
   ];
 }
